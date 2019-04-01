d2ed58e4b2cb2e95851c228843c8639041e87b8a
Red1one <49151728+Red1one@users.noreply.github.com>
2019-04-01 21:41:53 +0100

    Inscription
    btn suivant amenant vers l'activité d'inscription
    ajout des plugin butterknif

--- app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/MainActivity.java
+++ app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/MainActivity.java
@@ -1,13 +1,31 @@
 package com.spirit.force.gestion_des_depenses_revenus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnSuiv1)
+    Button btnSuiv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ButterKnife
+        ButterKnife.bind(this);
+        btnSuiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Inscription.class));
+            }
+        });
     }
 }
