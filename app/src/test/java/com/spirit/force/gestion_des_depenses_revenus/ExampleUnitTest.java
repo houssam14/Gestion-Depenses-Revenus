f75c5224c494443749c50f09f29277cb217c1241
houssam14 <adkinshoussam14@gmail.com>
2019-04-01 11:14:54 +0200

    first work

---
+++ app/src/test/java/com/spirit/force/gestion_des_depenses_revenus/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.spirit.force.gestion_des_depenses_revenus;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
