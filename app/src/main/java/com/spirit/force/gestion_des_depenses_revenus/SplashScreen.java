e2fe320300e6293d20b34cb5ba191ca53e46ff1d
houssam14 <adkinshoussam14@gmail.com>
2019-04-02 22:30:03 +0100

    Splash Screen2

--- app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/SplashScreen.java
+++ app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/SplashScreen.java
@@ -1,20 +1,33 @@
 package com.spirit.force.gestion_des_depenses_revenus;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pl.droidsonroids.gif.GifImageView;
 
 public class SplashScreen extends Activity {
+@BindView(R.id.imageView2)
+    ImageView logo;
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             // TODO Auto-generated method stub
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.splash);
+            setContentView(R.layout.spalsh);
+            ButterKnife.bind(this);
 
             Thread timerThread = new Thread(){
                 public void run(){
                     try{
+                        logo.setImageResource(R.drawable.splash2);
                         sleep(3000);
                     }catch(InterruptedException e){
                         e.printStackTrace();
