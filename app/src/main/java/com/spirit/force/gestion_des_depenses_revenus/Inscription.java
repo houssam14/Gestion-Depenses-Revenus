22a35a6bfbd5706fe8468b218aaaee85d1faea5b
Red1one <49151728+Red1one@users.noreply.github.com>
2019-04-02 09:18:15 +0100

    Insciption
    les composants de l'inscription sont complets;
    rest la verification du mot de passe et la connexion à la base de données

--- app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/Inscription.java
+++ app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/Inscription.java
@@ -1,13 +1,33 @@
 package com.spirit.force.gestion_des_depenses_revenus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Inscription extends AppCompatActivity {
-
+    @BindView(R.id.btnEnr)
+    Button  btnEn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+        //ButterKnife
+        ButterKnife.bind(this);
+
+
+        btnEn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SaisieRev.class));
+            }
+        });
+
+
     }
 }
