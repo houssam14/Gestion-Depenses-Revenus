af875e84dc91669403bc8a02d9257e5fe2f5d9db
houssam14 <adkinshoussam14@gmail.com>
2019-04-08 10:25:33 +0100

    About app

---
+++ app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/AproposDeLapp.java
@@ -0,0 +1,21 @@
+package com.spirit.force.gestion_des_depenses_revenus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AproposDeLapp extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos_de_lapp);
+
+    }
+}
--- app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/MainActivity.java
+++ app/src/main/java/com/spirit/force/gestion_des_depenses_revenus/MainActivity.java
@@ -1,11 +1,12 @@
 package com.spirit.force.gestion_des_depenses_revenus;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,13 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textpropos = findViewById(R.id.propos);
+        textpropos.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
 
         //ButterKnife
         ButterKnife.bind(this);
         btnSuiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Inscription.class));
+                startActivity(new Intent(getApplicationContext(), Inscription.class));
+            }
+        });
+
+
+        textpropos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AproposDeLapp.class);
+                startActivity(intent);
             }
         });
     }
